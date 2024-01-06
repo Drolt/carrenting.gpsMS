5c500ad8f92edbd0a715051bc3cdc2a7e66f652e
Drolt <thomas_klug@gmx.de>
2024-01-06 10:34:37 +0100

    initialize gpsMS

---
+++ gpsMS/src/main/java/com/carrenting/gpsMS/GpsMsApplication.java
@@ -0,0 +1,13 @@
+package com.carrenting.gpsMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsMsApplication.class, args);
+	}
+
+}
---
+++ gpsMS/src/test/java/com/carrenting/gpsMS/GpsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrenting.gpsMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
